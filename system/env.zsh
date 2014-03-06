63f2614f8427cc412f7d2a442789393b4ee1da37
Stian Grytøyr <stian@grytoyr.net>
2014-03-06 12:06:20 +0100

    Fixed EDITOR variable

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,14 @@
-export EDITOR='subl -w'
+
+if [[ `uname` == 'Darwin' ]]
+then
+  export EDITOR='subl -w'
+  alias emacs="s -w"
+  alias emasc="s -w"
+  alias cat="colorize"
+  # alias less="colorize"
+else
+  export EDITOR='emacs -nw'
+  alias emacs="emacs -nw"
+  alias emasc="emacs -nw"
+fi
+
