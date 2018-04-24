74056fc68c0a46a6a65987c13e2fd68251000369
Stian Grytøyr <stian@grytoyr.net>
2018-04-24 23:05:54 +0200

    Sign git commits

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -11,7 +11,7 @@ alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 #alias gp='git push origin HEAD'
 #alias gs="git status"
-alias gc="git commit -m "
+alias gc="git commit -S -m "
 alias gp="git push"
 alias ga="git add"
 alias gdt="git difftool"
